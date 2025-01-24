c487b7cf3e6b676d4cc24971cb225aebbaeb9470
Dibyendu Bar <dibyendubar@Dibyendus-MacBook-Air.local>
2025-01-24 11:21:31 +0530

    cors

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ require('dotenv').config();
 // Initialize express app
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+  origin: 'https://mini-lms-ui.netlify.app',  // Frontend URL
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',  // Allow headers
+  credentials: true,
+};
+
+app.use(cors(corsOptions));  // Apply CORS middleware
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve static files
 
 // MongoDB Atlas connection
